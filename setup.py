0492da143b9230eb769c76bcded1c45407cba689
Bauke <bauke.brenninkmeijer@gmail.com>
2019-09-02 15:47:23 +0200

    Update to release 1.0.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="table_evaluator",
-    version="0.0.3",
+    version="1.0.0",
     author="Bauke Brenninkmeijer",
     author_email="bauke.brenninkmeijer@gmail.com",
     description="A package to evaluate how close a synthetic data set is to real data.",
