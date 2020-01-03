0ab81cc29d80b32011b486d4a12691cc68991c2c
Bauke <bauke.brenninkmeijer@gmail.com>
2020-01-03 18:08:49 +0100

    this should work for now

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -50,6 +50,8 @@ extensions = [
     'm2r',
 ]
 
+suppress_warnings = ['image.not_readable']
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
