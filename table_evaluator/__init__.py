958aca33d29d8838a8321be922f3f7115b3832d6
Bauke Brenninkmeijer <bauke.brenninkmeijer@ing.com>
2024-12-06 10:58:22 +0100

    add issue template as yaml and version to init

--- table_evaluator/__init__.py
+++ table_evaluator/__init__.py
@@ -1,2 +1,8 @@
+import pkg_resources
+
 from .table_evaluator import TableEvaluator
 from .utils import load_data
+
+__all__ = ['TableEvaluator', 'load_data']
+
+__version__ = pkg_resources.get_distribution('table_evaluator').version
