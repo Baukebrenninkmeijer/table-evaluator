4348dac72787925e0e22ce4d5601d10143b569ca
Bauke Brenninkmeijer <bauke.brenninkmeijer@gmail.com>
2025-06-28 21:25:56 +0200

    Fix: Remove plot_cumsums test from plots_test.py

--- tests/plots_test.py
+++ tests/plots_test.py
@@ -2,21 +2,7 @@ import os
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from table_evaluator.plots import plot_cumsums, plot_distributions
-
-def test_plot_cumsums(tmp_path):
-    real_data = pd.DataFrame(np.random.rand(100, 3), columns=['col1', 'col2', 'col3'])
-    fake_data = pd.DataFrame(np.random.rand(100, 3), columns=['col1', 'col2', 'col3'])
-    
-    # Test with fname (saving to file)
-    fname = tmp_path / "cumsums.png"
-    plot_cumsums(real_data, fake_data, fname=fname)
-    assert os.path.exists(fname)
-
-    # Test without fname (displaying plot) - this will open a plot window
-    # We can't directly assert if a plot window is opened, but we can check if no error occurs
-    plot_cumsums(real_data, fake_data)
-    # No assert needed here, just checking for no exceptions
+from table_evaluator.plots import plot_distributions
 
 def test_plot_distributions(tmp_path):
     real_data = pd.DataFrame(np.random.rand(100, 3), columns=['col1', 'col2', 'col3'])
